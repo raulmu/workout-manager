f797c8968eda1f333cd4dbca23ff52ac0158203a
raulmu <raulmu@outlook.com>
2020-06-16 10:56:07 -0300

    SEMTAREFA Dependencies Adjusts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AddPage } from './pages/add/add.page';
@@ -18,12 +16,19 @@ import { GoogleAnalyticsService } from './services/google-analytics.service';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [AddPage],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AddPageModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AddPageModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    
+  ],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    GoogleAnalyticsService
+    GoogleAnalyticsService,
+
   ],
   bootstrap: [AppComponent]
 })
